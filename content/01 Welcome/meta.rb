65960ee480d7e13b9ff11390e3cb8e9c67c5a7cc
Marcus Crafter <crafterm@redartisan.com>
2009-05-21 10:25:08 +1000

    Playing with the idea of embedded content or being able to specify an alternate content file

--- content/01 Welcome/meta.rb
+++ content/01 Welcome/meta.rb
@@ -2,4 +2,10 @@ article :welcome do
   permalink '2007/9/3/welcome'
   posted '3 September 2007'
   tags :australia, :rails, :redartisan, :ruby
+  
+  # content 'mypost.markdown'
+  # content <<-EOF
+  #   this is the post info
+  # EOF
+
 end
