08f21c32bdc300a25cce8653593b941e90deedd9
Marcus Crafter <crafterm@redartisan.com>
2009-07-19 22:48:37 +1000

    Add Sprinkle annoucement article

---
+++ content/23 Sprinkle Some Powder/meta.rb
@@ -0,0 +1,7 @@
+article 'Sprinkle Some Powder!' do
+  permalink 'sprinkle-intro'
+  posted '2008/5/27'
+  tags :deployment, :installation, :merb, :provisioning, :rails, :ruby, :software, :sprinkle
+end
+
+
