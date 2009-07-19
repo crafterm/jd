6bc90aeb2fb2491dd5a39051b9940e7bef65c14f
Marcus Crafter <crafterm@redartisan.com>
2009-07-19 22:48:26 +1000

    Add Emacs Twilight theme article

---
+++ content/22 Twilight Theme for Emacs/meta.rb
@@ -0,0 +1,8 @@
+article 'Twilight Theme for Emacs!' do
+  permalink 'twilight-emacs'
+  posted '2008/5/27'
+  tags :colour, :editor, :emacs, :textmate, :theme, :twilight
+  attachments 'twilight_emacs.png'
+end
+
+
