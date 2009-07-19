839e7cb69cc8dcb686f36c637e6eb82af0525e10
Marcus Crafter <crafterm@redartisan.com>
2009-07-19 20:22:08 +1000

    Add RubyCocoa - Core Image Cocoaheads slides/article

---
+++ content/19 Ruby Cocoa & Core Image at Cocoaheads/meta.rb
@@ -0,0 +1,7 @@
+article 'Ruby Cocoa & Core Image at Cocoaheads' do
+  permalink 'rubycocoa-melbourne-cocoaheads'
+  posted '2008/4/11'
+  tags :cocoaheads, :core, :image, :melbourne, :rubycocoa
+end
+
+
