555b5a8112bb2188534649c989a6c185a6f6386a
Marcus Crafter <crafterm@redartisan.com>
2009-08-31 21:08:48 +1000

    Ensure sinatra-sinatra gem is used

--- jd.rb
+++ jd.rb
@@ -2,6 +2,7 @@ JD_ROOT = File.expand_path(File.dirname(__FILE__)) unless defined?(JD_ROOT)
 JD_CONTENT_ROOT = File.join(File.expand_path(File.dirname(__FILE__)), 'content') unless defined?(JD_CONTENT_ROOT)
 
 require 'rubygems'
+gem 'sinatra-sinatra'
 require 'sinatra'
 require 'lib/sinatra/redartisan/blog'
 require 'lib/sinatra/redartisan/content'
