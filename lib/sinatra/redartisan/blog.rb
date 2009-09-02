9a77232ab39bd59690a9b23d785c393b86f2b5a5
Marcus Crafter <crafterm@redartisan.com>
2009-09-03 08:04:16 +1000

    Trying 4 recent posts on the welcome page

--- lib/sinatra/redartisan/blog.rb
+++ lib/sinatra/redartisan/blog.rb
@@ -5,7 +5,7 @@ module Sinatra
         
         # GET /
         app.get '/' do
-          @posts = options.repository.recent(3)
+          @posts = options.repository.recent(4)
           erb :welcome
         end
         
