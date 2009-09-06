b47b662b6b87bad37c0402a182dfa5467f08ede5
Marcus Crafter <crafterm@Silver-Raptor.local>
2009-09-06 17:42:49 +1000

    Remove comments from content

--- content/01 Welcome/meta.rb
+++ content/01 Welcome/meta.rb
@@ -2,10 +2,4 @@ article 'Welcome!' do
   permalink 'welcome'
   posted '2007/9/3'
   tags :australia, :rails, :redartisan, :ruby
-  
-  # content 'mypost.markdown'
-  # content <<-EOF
-  #   this is the post info
-  # EOF
-
 end
