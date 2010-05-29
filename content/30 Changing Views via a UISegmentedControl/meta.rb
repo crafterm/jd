0b3bd0ae130a4f065726d193911578402c84f8d2
Marcus Crafter <crafterm@redartisan.com>
2010-05-29 11:55:30 +1000

    Remove example from content meta data, now hosted on github

--- content/30 Changing Views via a UISegmentedControl/meta.rb
+++ content/30 Changing Views via a UISegmentedControl/meta.rb
@@ -2,5 +2,5 @@ article 'Switching between Views with a UISegmentedControl' do
   permalink 'uisegmented-control-view-switching'
   posted '2010/5/26'
   tags :uikit, :development, :iphone, :ipad, :uisegmentedcontrol
-  attachments '1-italy.png', '2-australia.png', 'SegmentedControlExample.zip'
+  attachments '1-italy.png', '2-australia.png'
 end
